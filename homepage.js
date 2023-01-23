d1604e64dd85768572ba1fa956a663598d230120
HardeepNain <hnspnain@gmail.com>
2023-01-23 22:09:06 +0530

    Module 1 notes updated

--- homepage.js
+++ homepage.js
@@ -1,10 +1,15 @@
 let matchLink = "https://www.espncricinfo.com/series/ipl-2020-21-1210595";
+
 const request = require("request");
 const cheerio = require("cheerio");
 const getAllMatches = require("./allMatches");
+
+
 request(matchLink , function(err , res , data){
     processData(data);
 })
+
+
 function processData(html){
     let myDocument = cheerio.load(html);
     let aTag = myDocument(".widget-items.cta-link a");
